dbfe9769f91c76c0859f9cf6d4df276007d2b7f8
João <joaojose.neto4560@gmail.com>
2023-10-31 12:47:02 -0300

    qr code in python

---
+++ index.py
@@ -0,0 +1,17 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction= qrcode.constants.ERROR_CORRECT_L,
+    box_size=30,
+    border=2,
+    
+)
+
+
+qr.add_data('lindas')
+qr.make(fit=True)
+
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('qrcode.png')
---
+++ test.py
@@ -0,0 +1,8 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(819,578, duration=2)
+pyautogui.click(202,719, duration=2)
+pyautogui.click(424,982, duration=2)
+pyautogui.click(435,981, duration=2)
+pyautogui.write("viado")
